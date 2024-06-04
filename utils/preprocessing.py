abbe0b9f90ae2081ff77244cc87161a13e809fc0
Yujing Huang <yhuang43@mgh.harvard.edu>
2024-06-04 14:17:17 -0400

    implement randomly centered cropping   1. utils.preprocessing.apply_randomcrop()      introduce optional parameter 'mode', default is 'random' (random crop).      if mode == 'center', randomly select image center point and crop around that.         start_center = np.array(crop_size)/2         end_center = np.array(input_shape) - np.array(crop_size)/2         center_point = np.random.uniform(low=start_center, high=end_center).astype(int)   2. utils.preprocessing.apply_augmentations()      remove optional parameter 'crop_size', which can be retrieved from 'config' object

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -63,25 +63,28 @@ def apply_spatial_transform(image, label, voxsize):
     return transformed_image, transformed_label
 
 
-def apply_randomcrop(image, label, crop_size=None):
+def apply_randomcrop(image, label, crop_size=None, mode='random'):
     """Randomly crop input tensors to a given shape. The input tensors are expected to have shape [batch H W D]."""
 
     # assuming image and label have the same dimensions
     input_shape = image.shape[1:]
 
-    crop_max_val = np.array(input_shape) - np.array(crop_size)
-    start_coords = np.random.uniform(low=0, high=crop_max_val).astype(int)
-    end_coords = start_coords + np.array(crop_size)
-    #print(f"input_shape: {input_shape}, crop_max_val: {crop_max_val}, crop_size: {crop_size}, start_coords: {start_coords}, end_coords: {end_coords}")
+    if (mode == 'random'):
+        crop_max_val = np.array(input_shape) - np.array(crop_size)            
+        start_coords = np.random.uniform(low=0, high=crop_max_val).astype(int)
+        end_coords = start_coords + np.array(crop_size)
+        #print(f"apply_randomcrop({mode}) - input_shape: {input_shape}, crop_max_val: {crop_max_val}, crop_size: {crop_size}, start_coords: {start_coords}, end_coords: {end_coords}")
 
-    slicing = [slice(None)] + [slice(start, end) for start, end in zip(start_coords, end_coords)]
-    cropped_image = image[slicing]
-    cropped_label = label[slicing]
-
-    """
-    cropped_image = image[:, start_coords[0]:end_coords[0], start_coords[1]:end_coords[1], start_coords[2]:end_coords[2]]
-    cropped_label = label[:, start_coords[0]:end_coords[0], start_coords[1]:end_coords[1], start_coords[2]:end_coords[2]]
-    """
+        slicing = [slice(None)] + [slice(start, end) for start, end in zip(start_coords, end_coords)]
+        cropped_image = image[slicing]
+        cropped_label = label[slicing]
+    elif (mode == 'center'):
+        start_center = np.array(crop_size)/2
+        end_center = np.array(input_shape) - np.array(crop_size)/2
+        center_point = np.random.uniform(low=start_center, high=end_center).astype(int)
+        
+        cropped_image = voxynth.augment.apply_center_crop(image, crop_size, center_point=center_point)
+        cropped_label = voxynth.augment.apply_center_crop(label, crop_size, center_point=center_point)
     
     return cropped_image, cropped_label
 
@@ -131,7 +134,6 @@ def apply_augmentations(
     original_label,
     config,        
     voxsize,
-    crop_size=None,
     output_dir=None,
     save_volumes=None,
     augmentations_to_apply=None,
@@ -163,6 +165,10 @@ def apply_augmentations(
             "bias_field",
         ]
 
+    crop_size = None
+    if (config["preprocessing"].get("crop_size") != 'None'):
+            crop_size = config["preprocessing"].get("crop_size")
+            
     if "flipping" in augmentations_to_apply:
         flip_prob = config["preprocessing"].get("flip_prob")
         left_right_corresponding = None
@@ -219,7 +225,7 @@ def apply_augmentations(
 
     if "randomcrop" in augmentations_to_apply:
         if crop_size is not None:
-            image_tensor, label_tensor = apply_randomcrop(image_tensor, label_tensor, crop_size)
+            image_tensor, label_tensor = apply_randomcrop(image_tensor, label_tensor, crop_size, mode='random')
             if save_volumes is not None and output_dir is not None:
                 save_volume(
                     image_tensor,
@@ -230,6 +236,23 @@ def apply_augmentations(
                     label_tensor,
                     original_label,
                     os.path.join(output_dir, save_volumes + "_randomcropped_label.mgz"),
+                )
+        else:
+            raise ValueError("Crop size must be provided when using the 'cropping' augmentation.")
+
+    if "randomcrop_center" in augmentations_to_apply:
+        if crop_size is not None:
+            image_tensor, label_tensor = apply_randomcrop(image_tensor, label_tensor, crop_size, mode='center')
+            if save_volumes is not None and output_dir is not None:
+                save_volume(
+                    image_tensor,
+                    original_image,
+                    os.path.join(output_dir, save_volumes + "_randomcropped_center_image.mgz"),
+                )
+                save_volume(
+                    label_tensor,
+                    original_label,
+                    os.path.join(output_dir, save_volumes + "_randomcropped_center_label.mgz"),
                 )
         else:
             raise ValueError("Crop size must be provided when using the 'cropping' augmentation.")
