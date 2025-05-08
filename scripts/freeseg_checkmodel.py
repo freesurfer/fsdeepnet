bd87b5f562aad3670c9e749b95e8d04b2a597c1c
Yujing Huang <yhuang43@mgh.harvard.edu>
2025-05-08 11:37:21 -0400

    add freeseg_checkmodel.py option '--weight_outdir <>'   - output model trainable parameters in given checkpoint   - the option is ignored if it is not run with '--checkpoint <>'

--- scripts/freeseg_checkmodel.py
+++ scripts/freeseg_checkmodel.py
@@ -13,13 +13,15 @@ from freeseg.utils import get_class
 
 description = """
 Usage: check_model.py 
-       --config <config.yaml> | --checkpoint <checkpoint>
+       --config <config.yaml> | --checkpoint <checkpoint> [--weight_outdir <weight_outdir>]
        [--ndims <n>]
        [--input_shape <H W (D)>]
        [--cpu]
 
-       Use '--config <>' to create the network, or 
-           '--checkpoint <> to load a pre-trained network.
+       1. Use '--config <>' to create the network, or 
+              '--checkpoint <> to load a pre-trained network.
+       2. Use '--weight_outdir <>' to save trainable parameters in <checkpoint>.
+          The option is ignored if it is not run with '--checkpoint <>'.
 """
 
 # Configure logging settings
@@ -61,6 +63,10 @@ def main():
         n_channels = model_arch_dict["num_channels"]
     else:
         # Load the Trained Model
+        if not os.path.isfile(args.checkpoint):
+            logging.error('ERROR: file does not exist: %s' % args.checkpoint)
+            sys.exit(1)
+
         checkpoint = Checkpoint()
         checkpoint.load(args.checkpoint, device=device)
         assert checkpoint.model_arch_dict is not None, "Model architecture information not available."
@@ -76,6 +82,15 @@ def main():
     model = model_class(model_arch_dict).to(device)
     if (args.checkpoint is not None):
         model.load_state_dict(checkpoint.model_state_dict)
+        if (args.weight_outdir is not None):
+            # output model trainable parameters in given checkpoint
+            import numpy as np
+            if (not os.path.exists(args.weight_outdir)):
+                os.makedirs(args.weight_outdir)
+            for name, param in model.named_parameters():
+                if param.requires_grad:
+                    path_npy = f"{args.weight_outdir}/{name}.npy"
+                    np.save(path_npy, param.data.cpu().numpy())
 
     models.model_print(model)
     models.model_summary(model, (n_channels, *args.input_shape[-args.ndims:]), device=device, debug=True)
@@ -90,9 +105,10 @@ def argument_parse(description):
     # input/outputs
     parser.add_argument("--config", type=str, help="Path to the configuration file")
     parser.add_argument("--checkpoint", type=str, help="Path to a checkpoint file")
+    parser.add_argument("--weight_outdir", type=str, help="Directory to save trainable parameters in given checkpoint")
     parser.add_argument("--ndims", type=int, default=3, help="Number of image dimensions, 2D or 3D")
     parser.add_argument("--input_shape", nargs="+", type=int, default=(160,160,160), help="Network image input shape, ex. 160 160 160")
-    parser.add_argument("--cpu", action='store_true', help="Run on CPU.")
+    parser.add_argument("--cpu", action='store_true', help="Run on CPU")
 
     if len(sys.argv) < 3:
         parser.print_help()
