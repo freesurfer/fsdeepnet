257e0d353d57e690a8947c43ec3b4ebfdfbfe039
Yujing Huang <yhuang43@mgh.harvard.edu>
2025-08-16 19:19:46 -0400

    adjust tests/tf2torch/loadh5_synthseg.py to use latest code changes   - synthseg_2.0.pth is converted from tensorflow model /usr/local/freesurfer/8.0.0/models/synthseg_2.0.h5   - synthseg_config.yaml has the freeseg UNet configuration used for the conversion     set 'skip_connect: encoder' so the freeseg prediction looks better

--- tests/tf2torch/loadh5_synthseg.py
+++ tests/tf2torch/loadh5_synthseg.py
@@ -1,22 +1,38 @@
 import sys
+import logging
+import argparse
 import h5py
 import numpy as np
 import torch
 
 from freeseg.config import Config
 from freeseg.checkpoint import Checkpoint
+from freeseg.training import Training
 
 """
-Usage: fspython loadh5_synthseg.py <synthseg h5 model file> <freeseg config file for synthseg> <converted freeseg pytorch pth model file>
+Usage: fspython loadh5_synthseg.py 
+         --tf_model <synthseg h5 model file>
+         --config <freeseg config file for synthseg>
+         --torch_model_saveas <converted freeseg pytorch pth model file>
 Example:
-  loadh5_synthseg.py /autofs/cluster/scratch_wednesday/yh887/FS8.0.0-copy/models/synthseg_2.0.h5 
-                     /autofs/cluster/scratch_wednesday/yh887/training.freeseg/pitgland/configs/config_synthseg.yaml 
-                     /autofs/cluster/scratch_wednesday/yh887/training.freeseg/synthseg/synthseg-converted.pth
-"""
-model_file   = sys.argv[1]
-config_file  = sys.argv[2]
-model_saveas = sys.argv[3]
+  loadh5_synthseg.py --tf_model /usr/local/freesurfer/8.0.0/models/synthseg_2.0.h5
+                     --config   tests/tf2torch/synthseg_config.yaml 
+                     --torch_model_saveas synthseg_2.0.pth
 
+Notes: The following ML tools use the same UNet, these tensorflow models can be converted
+       to pytorch models with similar weight/bias mapping shown in this script.
+       Check each network configuration, adjust 'keys_model_state_dict' accordingly.
+
+              mri_easyreg/mri_easyreg
+              mri_synthseg/mri_synthseg
+              mri_synthsr/mri_synthsr
+              mri_segment_thalamic_nuclei_dti_cnn/mri_segment_thalamic_nuclei_dti_cnn
+              mri_sclimbic_seg/mri_sclimbic_seg
+              mri_segment_hypothalamic_subunits/mri_segment_hypothalamic_subunits
+              mri_synthsr/mri_synthsr_hyperfine
+              recon_all_clinical/mri_synth_surf
+              mri_claustrum_seg/mri_claustrum_seg
+"""
 keys_model_state_dict = {
 "unet_conv_downarm_0_0/unet_conv_downarm_0_0/kernel:0"  : "encoder.0.0.convs.0.0.weight", 
 "unet_conv_downarm_0_0/unet_conv_downarm_0_0/bias:0"    : "encoder.0.0.convs.0.0.bias", 
@@ -107,54 +123,81 @@ keys_model_state_dict = {
 "unet_likelihood/unet_likelihood/bias:0"                : "classifier.bias"
 }
 
-config = Config.load(config_file)
-
-# create training/validation dataset with the desired augmentations specified
-labels_segmentation = sorted(config["dataset"]["expected_classes"])
-label_mapping = {label:i for i, label in enumerate(labels_segmentation)}
-inverse_label_mapping = {v: k for k, v in label_mapping.items()}
-#config["dataset"]["label_mapping"] = label_mapping
-train_dataset_dict = {
-    "batch_size": config["training"]["batch_size"],
-    "segmentation_labels" : labels_segmentation,
-    "label_mapping": label_mapping,
-    "inverse_label_mapping": inverse_label_mapping,
-    "crop_size"    : config["preprocessing"]["crop_size"],    
-    "num_channels" : 1,
-}
-model_arch_dict = config["model"]
-model_arch_dict["num_channels"] = config["dataset"]["expected_num_channels"]
-model_arch_dict["nb_labels"] = len(config["dataset"]["expected_classes"])
-model_arch_dict["bn_track_running_stats"] = True
-#model_arch_dict["add_priors"] = train_dataset_dict.get("priors", False)
-
-checkpoint = Checkpoint(model_arch_dict=model_arch_dict, train_dataset_dict=train_dataset_dict, label_lookup=None) 
-model_state_dict = {}
-
-def preview_hdf5(name, obj):
-    if (isinstance(obj, h5py.Dataset)):
-        dict_key = keys_model_state_dict.get(name)
-        if (dict_key is None):
-            print(f"Dataset: {name} {obj.shape} - skip")
-        else:
-            print(f"Dataset: {name} {obj.shape} {dict_key}")
-
-            numpy_array = np.zeros(obj.shape, dtype=obj.dtype)
-            obj.read_direct(numpy_array)
-            torch_tensor = torch.tensor(numpy_array)
-            if (torch_tensor.ndim == 5):
-                torch_tensor = torch_tensor.permute(4, 3, 0, 1, 2)
-            model_state_dict[dict_key] = torch_tensor
+# Configure logging settings
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the log level (e.g., DEBUG, INFO, WARNING, ERROR)
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler(),  # Print to terminal
+    ],
+)
 
 
-with h5py.File(model_file, 'r') as f:
-    f.visititems(preview_hdf5)
+def main():
+    def preview_hdf5(name, obj):
+        if (isinstance(obj, h5py.Dataset)):
+            dict_key = keys_model_state_dict.get(name)
+            if (dict_key is None):
+                print(f"Dataset: {name} {obj.shape} - skip")
+            else:
+                print(f"Dataset: {name} {obj.shape} {dict_key}")
 
-# model_dict
-model_dict = {"model_state_dict" : model_state_dict}
-checkpoint.save(model_saveas, model_dict)
+                numpy_array = np.zeros(obj.shape, dtype=obj.dtype)
+                obj.read_direct(numpy_array)
+                torch_tensor = torch.tensor(numpy_array)
+                if (torch_tensor.ndim == 5):
+                    torch_tensor = torch_tensor.permute(4, 3, 0, 1, 2)
+                model_state_dict[dict_key] = torch_tensor
 
-print(f"Synthseg model saved as {model_saveas}")
+
+    args = argument_parse()
+
+    tf_model_file = args.tf_model
+    torch_model_saveas = args.torch_model_saveas
+    
+    config = Config.process(args, logger=logging, require_train_outfolder=False, require_dataset_list=False)
+    config, _, _, model, _, _ = Training.setup(config, preload_dataset=False, create_train_dataset=False, create_loader=False, create_model=True)
+
+    model_arch_dict = model.arch_dict
+    logging.info(f"{model_arch_dict.get('name')}:")    
+    for k in model_arch_dict.keys():
+        logging.info(f"    {k}: {model_arch_dict[k]}")
+    logging.info("")
+    
+    train_dataset_dict = config["dataset"]
+    label_lookup = train_dataset_dict.get("label_lookup", None)
+    checkpoint = Checkpoint(model_arch_dict=model_arch_dict, train_dataset_dict=train_dataset_dict, label_lookup=None) 
+
+    model_state_dict = {}
+    with h5py.File(tf_model_file, 'r') as f:
+        f.visititems(preview_hdf5)
+
+    # model_dict
+    model_dict = {"model_state_dict" : model_state_dict}
+    checkpoint.save(torch_model_saveas, model_dict)
+
+    print(f"Synthseg pytorch model saved as {torch_model_saveas}")
+
+
+def argument_parse():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+
+    # input/outputs
+    parser.add_argument("--config", type=str, required=True, help="Path to the configuration file")
+    parser.add_argument("--tf_model", type=str, help="Path to tensorflow model file")
+    parser.add_argument("--torch_model_saveas", type=str, help="Path to converted pytorch checkpoint")
+
+    # parse commandline
+    args = parser.parse_args()
+
+    return args
+
+
+# execute script
+if __name__ == '__main__':
+    main()
+
 
 def preview_hdf5_0(name, obj):
     if (isinstance(obj, h5py.Group)):
