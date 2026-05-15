6bf0d197806b39b0b65bd542894ecb95cd435cb8
Yujing Huang <yhuang43@mgh.harvard.edu>
2026-05-15 11:22:27 -0400

    add '--model_layer_mapping' option to load tf-pytoch model layer mapping

--- tests/tf2torch/loadh5_synthseg.py
+++ tests/tf2torch/loadh5_synthseg.py
@@ -12,18 +12,19 @@ from freeseg.training import Training
 
 """
 Usage: fspython loadh5_synthseg.py 
-         --tf_model <synthseg h5 model file>
-         --preview | --config <freeseg config file for synthseg> --torch_model_saveas <converted freeseg pytorch pth model file>
+         --tf_model <tensorflow h5 model file>
+         --preview | --config <freeseg model config> --model_layer_mapping <tf-pytoch model layer mapping> --torch_model_saveas <converted pytorch pth model file>
 Example:
   loadh5_synthseg.py --tf_model /usr/local/freesurfer/8.0.0/models/synthseg_2.0.h5
                      --config   tests/tf2torch/synthseg_config.yaml 
+                     --model_layer_mapping tests/tf2torch/synthseg_tf2torch_mapping.yaml
                      --torch_model_saveas synthseg_2.0.pth
 
 Notes: The following ML tools use the same UNet, these tensorflow models can be converted
        to pytorch models with similar weight/bias mapping shown in this script.
        Check each network configuration, adjust 'keys_model_state_dict' accordingly.
 
-              mri_easyreg/mri_easyreg
+              mri_easyreg/mri_easyreg  (this also uses voxelmorph)
               mri_synthseg/mri_synthseg
               mri_synthsr/mri_synthsr
               mri_segment_thalamic_nuclei_dti_cnn/mri_segment_thalamic_nuclei_dti_cnn
@@ -33,6 +34,10 @@ Notes: The following ML tools use the same UNet, these tensorflow models can be 
               recon_all_clinical/python/mri_synth_surf.py
               mri_claustrum_seg/mri_claustrum_seg
 """
+
+"""
+# example: model layer mapping for 5 level synthseg network
+# use --model_layer_mapping <tf-pytoch model layer mapping> to pass the dictionary
 
 keys_model_state_dict = {
 "unet_conv_downarm_0_0/unet_conv_downarm_0_0/kernel:0"  : "encoder.0.0.convs.0.0.weight", 
@@ -123,6 +128,9 @@ keys_model_state_dict = {
 "unet_likelihood/unet_likelihood/kernel:0"              : "classifier.weight", 
 "unet_likelihood/unet_likelihood/bias:0"                : "classifier.bias"
 }
+"""
+keys_model_state_dict = None
+
 
 # Configure logging settings
 logging.basicConfig(
@@ -159,18 +167,21 @@ def main():
             f.visititems(preview_hdf5)
         sys.exit(0)
 
-    assert (args.config is not None and args.torch_model_saveas is not None), \
-        "'--config <>' and '--torch_model_saveas <>'are required to convert tensorflow model"
+    assert (args.config is not None and args.model_layer_mapping is not None and args.torch_model_saveas is not None), \
+        "'--config <>', '--model_layer_mapping <>', and '--torch_model_saveas <>'are required to convert tensorflow model"
     torch_model_saveas = args.torch_model_saveas
+    keys_model_state_dict = Config.load(args.model_layer_mapping)
     
     config = Config.process(args, logger=logging, require_train_outfolder=False, require_dataset_list=False)
-    config, _, _, _, model, _, _, _ = Training.setup(config, preload_dataset=False, create_train_dataset=False, create_loader=False, create_model=True)
+    config, _, _, _, model, _, _, _ = Training.setup(config, preload_dataset=False, create_train_dataset=False, create_loader=False, create_model=True, set_dataset_attr=False)
 
     models.model_arch(model.arch_dict, logger=logging)
     
-    train_dataset_dict = config["dataset"]
-    label_lookup = train_dataset_dict.get("label_lookup", None)
-    checkpoint = Checkpoint(model_arch_dict=model.arch_dict, train_dataset_dict=train_dataset_dict, label_lookup=None) 
+    label_lookup = None
+    train_dataset_dict = config.get("dataset", None)
+    if (train_dataset_dict is not None):
+        label_lookup = train_dataset_dict.get("label_lookup", None)
+    checkpoint = Checkpoint(model_arch_dict=model.arch_dict, train_dataset_dict=train_dataset_dict, label_lookup=label_lookup) 
 
     # skip moving_mean/moving_variance if track_running_stat=False
     skip_moving_mean_variance = not model.arch_dict["track_running_stats"]
@@ -183,7 +194,7 @@ def main():
     model_dict = {"model_state_dict" : model_state_dict}
     checkpoint.save(torch_model_saveas, model_dict)
 
-    print(f"Synthseg pytorch model saved as {torch_model_saveas}")
+    print(f"converted model saved as {torch_model_saveas}")
 
 
 def argument_parse():
@@ -193,6 +204,7 @@ def argument_parse():
     # input/outputs
     parser.add_argument("--preview", action='store_true', help="Preview tensorflow model layers")
     parser.add_argument("--config", type=str, help="Path to the configuration file")
+    parser.add_argument("--model_layer_mapping", type=str, help="Path to tf-pytoch model layer mapping")
     parser.add_argument("--tf_model", type=str, required=True, help="Path to tensorflow model file")
     parser.add_argument("--torch_model_saveas", type=str, help="Path to converted pytorch checkpoint")
 
